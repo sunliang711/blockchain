24d9cf7a888bf79a3fc3111897b95c941456cbeb
sunliang <sunliang711@163.com>
2021-06-15 19:13:56 +0800

    add start-console-log.sh all in one script

--- deploy/eth/04-console.sh
+++ deploy/eth/04-console.sh
@@ -8,6 +8,10 @@ cd "$root"
 # write your code below
 
 source "loadConfig.sh" 2>/dev/null || { echo "loadConfig.sh not found."; exit 1; }
-
+echo "rpc port:$port"
+while ! nc -z 127.0.0.1 $port >/dev/null 2>&1;do
+    echo "wait rpc port working..."
+    sleep 2
+done
 echo "attach to node with datadir: ${datadir}..."
 geth --datadir "${datadir}" attach
